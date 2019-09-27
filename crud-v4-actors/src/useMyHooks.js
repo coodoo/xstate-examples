27a467d4d39e7236677e153439ae9c0d3d61828d
Jeremy Lu <jx@twmug.com>
2019-09-27 10:34:44 +0800

    bak

--- crud-v4-actors/src/useMyHooks.js
+++ crud-v4-actors/src/useMyHooks.js
@@ -13,7 +13,7 @@ export const useMachineEx = (machine, { debug=false, name='', interpreterOptions
 	if(machineRef.current !== machine){
 
 		machineRef.current = machine
-console.log( '跑',  )
+
 		serviceRef.current = interpret(machineRef.current, interpreterOptions)
 		.onTransition( state => {
 
